a1bb745cf4fc13bba49c3777c07ce38c4c78bf33
muratso <rockstar2304@gmail.com>
2016-09-16 09:08:36 -0300

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp -f /otrs.cnf /etc/mysql/conf.d/otrs.cnf
 cp /opt/otrs/var/cron/otrs_daemon.dist /opt/otrs/var/cron/otrs_daemon
-/opt/otrs/bin/Cron.sh start otrs && su -c "bin/otrs.Daemon.pl start" -s /bin/bash otrs
+/opt/otrs/bin/Cron.sh start otrs && su -c "/opt/otrs/bin/otrs.Daemon.pl start" -s /bin/bash otrs
 apache2ctl -D FOREGROUND
 
